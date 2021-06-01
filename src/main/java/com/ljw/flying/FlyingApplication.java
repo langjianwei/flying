bd73f1658fd20eac319b7b86862cd6fe5ea3cd8e
郎建伟-优捷 <il>
2021-06-01 15:55:14 +0800

    Initial commit

---
+++ src/main/java/com/ljw/flying/FlyingApplication.java
@@ -0,0 +1,13 @@
+package com.ljw.flying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyingApplication.class, args);
+    }
+
+}
