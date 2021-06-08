fc3b3db13065884c2fb51797f1028380ddb51428
郎建伟-优捷 <il>
2021-06-08 16:08:41 +0800

    测试post请求

--- src/test/java/com/ljw/flying/FlyingApplicationTests.java
+++ src/test/java/com/ljw/flying/FlyingApplicationTests.java
@@ -5,15 +5,29 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class FlyingApplicationTests {
 
 
     @Test
-    public void testHC5() {
+    public void testHC5Get() {
         HC5AsyncDownImpl async = new HC5AsyncDownImpl(10000, Charset.defaultCharset());
         String html = async.down("http://idea.94goo.com/key");
+        System.out.println(html);
+    }
+    @Test
+    public void testHC5Post() {
+        HC5AsyncDownImpl async = new HC5AsyncDownImpl(10000, Charset.defaultCharset());
+        Map<String ,Object> params = new HashMap<>();
+        params.put("doctitle","习声回响");
+        params.put("page","2");
+        params.put("size",10);
+        Map<String ,Object> headers = new HashMap<>();
+        headers.put("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36");
+        String html = async.down("http://s.cnr.cn/search", "POST", params, headers);
         System.out.println(html);
     }
 
